12c00f67b7b2af864b7668e5fae525ebd56b8d2d
Hamza Aamir <hamzaamirm2001@gmail.com>
2022-08-13 07:39:23 +0500

    minor updates

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,10 +3,10 @@
 $(document).ready(function() {
 	var $btns = $('.search_tabs').click(function() {
 	  if (this.id == 'all') {
-	    $('#parent > section').fadeIn(450);
+	    $('#main > section').fadeIn(450);
 	  } else {
 	    var $el = $('.' + this.id).fadeIn(450);
-	    $('#parent > section').not($el).hide();
+	    $('#main > section').not($el).hide();
 	  }
 	  $btns.removeClass('active');
 	  $(this).addClass('active');
